0ca73798c25f30a15eeaf6bbb1165d0e60fb2fe1
Artur <pufereq@gmail.com>
2023-03-17 23:23:24 +0100

    feat(init): add init.py
    add curses initialization functions.
    - init_stdscr() initializes curses,
    - init_content_win() initializes main content window,
    - init_curses() is the main initialization function; calls init_stdscr(),
      init_content_win() and init_topbar().

---
+++ simulat/core/init.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import curses as cs
+
+from simulat.core.windows.topbar import init_topbar
+
+
+def init_stdscr():
+    global stdscr, stdscr_height, stdscr_width
+
+    stdscr = cs.initscr()
+    stdscr_height, stdscr_width = stdscr.getmaxyx()
+
+    cs.noecho()
+    cs.cbreak()
+    cs.curs_set(0)
+    cs.start_color()
+
+    return stdscr
+
+
+def init_content_win():
+    global content_win, content_win_height, content_win_width
+
+    content_win = cs.newwin(stdscr_height - 1, stdscr_width, 1, 0)
+    content_win_height, content_win_width = content_win.getmaxyx()
+
+
+def init_curses():
+    stdscr = init_stdscr()
+    init_topbar(stdscr)
+    init_content_win()
