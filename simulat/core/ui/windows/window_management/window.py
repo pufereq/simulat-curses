a820d55e14369f89cd5ef95a2affd4b1fbac8e56
Artur Borecki <pufereq@gmail.com>
2023-06-13 22:14:25 +0200

    refactor(window.py): fix whitespace in function definitions

--- simulat/core/ui/windows/window_management/window.py
+++ simulat/core/ui/windows/window_management/window.py
@@ -23,17 +23,16 @@ class Window():
         self.window.refresh()
 
     def border(self,
-               ls = 0, rs = 0,
-               ts = 0, bs = 0,
-               tl = 0, tr = 0,
-               bl = 0, br = 0):
+               ls=0, rs=0,
+               ts=0, bs=0,
+               tl=0, tr=0,
+               bl=0, br=0):
         self.window.border(ls, rs, ts, bs, tl, tr, bl, br)
         self.refresh()
 
-    def subwin(self, nlines: int, ncols:int, y: int, x: int, reverse: bool = False):
+    def subwin(self, nlines: int, ncols: int, y: int, x: int, reverse: bool = False):
         from .subwindow import SubWindow
         return SubWindow(self.window, nlines, ncols, y, x, reverse)
 
     def getmaxyx(self):
         return self.window.getmaxyx()
-
