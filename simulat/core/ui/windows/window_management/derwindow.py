7a009d17e3e826f46dd42553be04ff8e3f6cb0f2
Artur <pufereq@gmail.com>
2023-07-24 10:01:35 +0200

    feat(derwindow.py): make make_panel and reverse named arguments

--- simulat/core/ui/windows/window_management/derwindow.py
+++ simulat/core/ui/windows/window_management/derwindow.py
@@ -6,7 +6,7 @@ from .subwindow import SubWindow
 
 
 class DerWindow(SubWindow):
-    def __init__(self, parent_window, nlines: int, ncols: int, rel_y: int, rel_x: int, make_panel: bool = True, reverse: bool = False):
+    def __init__(self, parent_window, nlines: int, ncols: int, rel_y: int, rel_x: int, *, make_panel: bool = False, reverse: bool = False):
 
         self.window = parent_window.derwin(nlines, ncols, rel_y, rel_x)
 
