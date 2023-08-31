6c729486334ba859f9192dcf5300e433a7354682
Artur Borecki <pufereq@gmail.com>
2023-08-31 13:34:40 +0200

    feat(init.py): add color support

--- simulat/core/init.py
+++ simulat/core/init.py
@@ -27,8 +27,64 @@ def init_content_win():
     content_win_height, content_win_width = content_win.getmaxyx()
 
 
+def init_colors():
+    global COLLIDER_COLOR, GRASS_COLOR, FLOOR_COLOR, INTERACTION_COLOR, INTERACTION_RADIUS_COLOR, PLAYER_COLOR, EMPTY_COLOR
+    COLLIDER_PAIR: int = 1
+    GRASS_PAIR: int = 2
+    FLOOR_PAIR: int = 3
+    INTERACTION_PAIR: int = 4
+    INTERACTION_RADIUS_PAIR: int = 5
+    PLAYER_PAIR: int = 6
+    EMPTY_PAIR: int = 7
+
+    COLOR_TRANSPARENT: int = -1
+
+    # colors
+    COLOR_BLACK: int = 0
+    COLOR_RED: int = 1
+    COLOR_GREEN: int = 2
+    COLOR_YELLOW: int = 3
+    COLOR_BLUE: int = 4
+    COLOR_MAGENTA: int = 5
+    COLOR_CYAN: int = 6
+    COLOR_WHITE: int = 7
+    COLOR_GRAY: int = 8
+
+    # bright colors
+    COLOR_BRIGHT_RED: int = 9
+    COLOR_BRIGHT_GREEN: int = 10
+    COLOR_BRIGHT_YELLOW: int = 11
+    COLOR_BRIGHT_BLUE: int = 12
+    COLOR_BRIGHT_MAGENTA: int = 13
+    COLOR_BRIGHT_CYAN: int = 14
+    COLOR_BRIGHT_WHITE: int = 15
+
+    # other colors
+    COLOR_BROWN: int = 136
+    COLOR_GRASS_GREEN: int = 22
+
+    cs.use_default_colors()
+
+    cs.init_pair(COLLIDER_PAIR, COLOR_GRAY, COLOR_WHITE)
+    cs.init_pair(GRASS_PAIR, COLOR_BRIGHT_GREEN, COLOR_GRASS_GREEN)
+    cs.init_pair(FLOOR_PAIR, COLOR_BRIGHT_YELLOW, COLOR_BROWN)
+    cs.init_pair(INTERACTION_PAIR, COLOR_CYAN, COLOR_CYAN)
+    cs.init_pair(INTERACTION_RADIUS_PAIR, COLOR_BRIGHT_CYAN, COLOR_BRIGHT_BLUE)
+    cs.init_pair(PLAYER_PAIR, COLOR_BRIGHT_MAGENTA, COLOR_TRANSPARENT)
+    cs.init_pair(EMPTY_PAIR, COLOR_WHITE, COLOR_TRANSPARENT)
+
+    COLLIDER_COLOR = cs.color_pair(COLLIDER_PAIR) | cs.A_NORMAL
+    GRASS_COLOR = cs.color_pair(GRASS_PAIR)
+    FLOOR_COLOR = cs.color_pair(FLOOR_PAIR)
+    INTERACTION_COLOR = cs.color_pair(INTERACTION_PAIR)
+    INTERACTION_RADIUS_COLOR = cs.color_pair(INTERACTION_RADIUS_PAIR)
+    PLAYER_COLOR = cs.color_pair(PLAYER_PAIR) | cs.A_BOLD
+    EMPTY_COLOR = cs.color_pair(EMPTY_PAIR)
+
+
 def init_curses():
     stdscr = init_stdscr()
+    init_colors()
     init_content_win()
     init_topbar(stdscr)
 
