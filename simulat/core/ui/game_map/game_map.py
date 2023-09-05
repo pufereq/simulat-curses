cda006f9dd5d0f6624c03cfa4ac277484123d4e2
Artur Borecki <pufereq@gmail.com>
2023-09-05 09:36:13 +0200

    feat(game_map.py): modify `GameMap()` class to use cell classes

--- simulat/core/ui/game_map/game_map.py
+++ simulat/core/ui/game_map/game_map.py
@@ -20,7 +20,9 @@ from simulat.core.ui.windows.window_management.pad import Pad
 
 from ....data.map_layouts.map_layout import MAP_LAYOUT, INTERACTIONS, TITLE
 
-from .door import Door
+from .cell.dynamic import DoorCell
+from .cell.decorative import GrassCell, FloorCell
+from .cell.collider import WallCell
 
 
 class GameMap():
@@ -28,21 +30,14 @@ class GameMap():
         self.MAP_SIZE: Final = 100, 100  # y, x
         self.pad_size = self.MAP_SIZE[0] + 2, self.MAP_SIZE[1] + 2  # subject to change when user resizes terminal
 
+        self.map = Pad(*self.pad_size)
+        content_win.panel.replace(self.map.window)
+
         self.doors = {}
         self.interactions = INTERACTIONS
 
-        self.map_layout = self._map_init(MAP_LAYOUT)
-        self.collision_matrix = self._collision_matrix_init(self.map_layout)
         self.title = TITLE
         self.movement_delay = 0.0  # seconds
-
-        self.grass_chars = ['`', "'", '"', ',', '.', ':', ';', '|', '¦']
-        grass_density = 100  # lesser = more grass
-        self.grass_chars.extend(' ' for _ in range(grass_density))
-
-        self.floor_chars = ['.', '_', '-']
-        floor_density = 15  # lesser = more knots
-        self.floor_chars.extend(' ' for _ in range(floor_density))
 
         self.player_char = '@'
         self.player_pos = 1, 1
@@ -55,15 +50,14 @@ class GameMap():
 
         self.max_displayed_pad_size = stdscr.getmaxyx()[0] - 1, stdscr.getmaxyx()[1] - 1
 
-        self.map = Pad(*self.pad_size)
-        content_win.panel.replace(self.map.window)
-
         # set title
         topbar.title_win.clear()
         topbar.title_win.addstr(0, -1, self.title)
         topbar.title_win.refresh()
 
         # init map
+        self.map_layout = self._map_init(MAP_LAYOUT)
+        self.collision_matrix = self._collision_matrix_init(self.map_layout)
         self._draw_map()
         self._refresh_map()
 
@@ -84,17 +78,37 @@ class GameMap():
         for y in range(self.pad_size[0] - 1):
             line = []
             for x in range(self.pad_size[1] - 1):
-                line.append('g')
+                # line.append('g')
+                line.append(GrassCell(self, y, x))
             layout.append(line)
 
         for y_idx, line in enumerate(_layout):
             for x_idx, char in enumerate(line):
-                layout[y_idx][x_idx] = char
+                # layout[y_idx][x_idx] = char
+
+                # doors
                 if char == "d" or char == "D":
-                    self.doors[(y_idx, x_idx)] = Door(y_idx, x_idx, char == "D")
-                    _door = self.doors[(y_idx, x_idx)]
+                    layout[y_idx][x_idx] = DoorCell(self, y_idx, x_idx, locked=char == "D")
+                    _door = layout[y_idx][x_idx]
                     if not _door.locked:
                         self.interactions[(y_idx, x_idx)] = _door.toggle
+                    self.doors[(y_idx, x_idx)] = _door
+
+                # walls
+                elif char == "#":
+                    layout[y_idx][x_idx] = WallCell(self, y_idx, x_idx)
+
+                # floor
+                elif char == "f":
+                    layout[y_idx][x_idx] = FloorCell(self, y_idx, x_idx)
+
+                # grass
+                elif char == "g":
+                    layout[y_idx][x_idx] = GrassCell(self, y_idx, x_idx)
+
+                # # empty
+                # else:
+                #     layout[y_idx][x_idx] = Cell(self, y_idx, x_idx)
 
             # layout.append(line_list)
         return layout
@@ -118,32 +132,21 @@ class GameMap():
         for y, line in enumerate(self.map_layout):
             for x, char in enumerate(line):
                 # if cell is a collider (1), draw it in COLLIDER_COLOR
-                if self.collision_matrix[y][x]:
-                    self.map.cs_addstr(y, x, char, COLLIDER_COLOR)
+                # if self.collision_matrix[y][x]:
+                #     self.map.cs_addstr(y, x, char, COLLIDER_COLOR)
 
                 # if cell is an interaction, store its range's coordinates in radius_coordinates
                 # and draw it in INTERACTION_COLOR
-                elif (y, x) in self.interactions:
+                if (y, x) in self.interactions:
                     for y_offset in [-1, 0, 1]:
                         for x_offset in [-1, 0, 1]:
                             y_radius = y + y_offset
                             x_radius = x + x_offset
                             radius_coordinates.append((y_radius, x_radius))
-                    self.map.cs_addstr(y, x, char, INTERACTION_COLOR)
+                    self.map.cs_addstr(y, x, str(char), INTERACTION_COLOR)
 
-                # if cell is a floor, draw it in FLOOR_COLOR
-                elif char == 'f':  # floor
-                    _floor_char = random.choice(self.floor_chars)
-                    self.map.cs_addstr(y, x, _floor_char, FLOOR_COLOR)
-
-                # if cell is grass, draw it in GRASS_COLOR
-                elif char == 'g':  # grass
-                    grass_char = random.choice(self.grass_chars)
-                    self.map.cs_addstr(y, x, grass_char, GRASS_COLOR)
-
-                # if cell is empty, draw it in EMPTY_COLOR
-                else:
-                    self.map.cs_addstr(y, x, char, EMPTY_COLOR)
+                char.draw()
+        self._refresh_map()
 
         # draw interaction radius
         for y, x in radius_coordinates:
@@ -156,13 +159,10 @@ class GameMap():
 
     def _draw_dynamic(self):
         # draw doors
-        for y, x in self.doors:
-            if self.doors[(y, x)].open:  # if door is open
-                self.map.cs_addstr(y, x, self.door_chars['open'], DOOR_COLOR)
-            elif not self.doors[(y, x)].locked:  # if door is closed and unlocked
-                self.map.cs_addstr(y, x, self.door_chars[self.map_layout[y][x]], DOOR_COLOR)
-            else:  # if door is closed and locked
-                self.map.cs_addstr(y, x, self.door_chars[self.map_layout[y][x]], LOCKED_DOOR_COLOR)
+        for door in self.doors:
+            self.doors[door].update()
+            self.doors[door].draw()
+        self._refresh_map()
 
     def _resize(self):
         self.max_displayed_pad_size = stdscr.getmaxyx()[0] - 1, stdscr.getmaxyx()[1] - 1
@@ -210,9 +210,12 @@ class GameMap():
         except IndexError:
             return
 
-        if self.doors.get((new_y, new_x)):
-            if not self.doors[(new_y, new_x)].open:
-                return
+        # if self.doors.get((new_y, new_x)):
+        #     if not self.doors[(new_y, new_x)].open:
+        #         return
+
+        if self.map_layout[new_y][new_x].collider:
+            return
 
         if not self.collision_matrix[new_y][new_x]:
             # Erase the player character from the current position
@@ -260,4 +263,5 @@ class GameMap():
 
         if closest_interaction is not None:
             self.interactions[closest_interaction]()
+            self._draw_dynamic()
             self._refresh_map()
