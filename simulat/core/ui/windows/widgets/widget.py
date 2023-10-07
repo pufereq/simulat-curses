31e5f57ead3bbd5ca00d2a4bab323a123fb07449
Artur <pufereq@gmail.com>
2023-10-07 13:03:21 +0200

    docs(widget.py): add docstrings to `Widget` class

--- simulat/core/ui/windows/widgets/widget.py
+++ simulat/core/ui/windows/widgets/widget.py
@@ -12,12 +12,27 @@ class WidgetLoopEnd(Exception):
 
 
 class Widget(DerWindow):
+    """A widget that can be displayed in a container window."""
     def __init__(self, parent):
+        """A widget that can be displayed in a container window.
+
+        Args:
+            parent (Container): The parent container window.
+        """
         super().__init__(parent.window, parent.max_y - 4, parent.max_x - 2, 3, 1, make_panel=False)
 
         self.result = None
 
-    def _wrap_str_to_width(self, text: str):
+    def _wrap_str_to_width(self, text: str) -> str:
+        """Wraps a string to the width of the widget.
+
+        Args:                self.window.insch(y, x, char)
+
+            text (str): The text to wrap.
+
+        Returns:
+            str: The wrapped text.
+        """
         lines = text.split('\n')
         new_lines = []
         for line in lines:
@@ -26,4 +41,11 @@ class Widget(DerWindow):
         return '\n'.join(new_lines)
 
     def _input(self, key: int):
+        """Handles the input of the widget.
+
+        Does nothing by default.
+
+        Args:
+            key (int): The key that was pressed.
+        """
         pass  # for static widgets
